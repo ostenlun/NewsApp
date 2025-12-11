8dd05f2e8a501285f20933340a1fbf7f6a658a5f
Olli Stenlund <olli.stenlund@gmail.com>
2025-12-11 10:19:45 +0000

    Removed unused code

--- src/main/java/com/codermages/NewsApp/controllers/NewsController.java
+++ src/main/java/com/codermages/NewsApp/controllers/NewsController.java
@@ -27,41 +27,4 @@ public class NewsController {
         model.addAttribute("description", "Description: " + article.getDescription());
         return "index";
     }
-
-    // @GetMapping("/index")
-    // public String index() {
-    //     System.out.println("Calling index");
-    //     return "index.html";
-    // }
-
-    @GetMapping("/error")
-    public String errorPage() {
-        return "<html lang=\"en\">\n" +
-"\n" +
-"<head>\n" +
-"    <meta charset=\"UTF-8\">\n" +
-"    <meta name=\"viewport\" \n" +
-"          content=\"width=device-width, \n" +
-"                   initial-scale=1.0\">\n" +
-"    <title>\n" +
-"        Error occurred\n" +
-"    </title>\n" +
-"    <link rel=\"stylesheet\" \n" +
-"          href=\"style.css\">\n" +
-"</head>\n" +
-"\n" +
-"<body>\n" +
-"    <div class=\"error-container\">\n" +
-"        <h1> Error </h1>\n" +
-"        <p>\n" +
-"            Oops! The page you're\n" +
-"            looking for is not here.\n" +
-"        </p>\n" +
-"    </div>\n" +
-"</body>\n" +
-"\n" +
-"</html>";
-    }
-    
-
 }
